ed121ad61fb022848317e73b42fc1b4b2771724c
Cadie Stockman <cadie.brown@gmail.com>
2021-04-01 23:16:26 -0400

    Adds ACF custom fields

--- includes/ucf-sources-taxonomy.php
+++ includes/ucf-sources-taxonomy.php
@@ -81,5 +81,76 @@ if ( ! class_exists( 'UCF_Sources_Taxonomy' ) ) {
 
 			return $args;
 		}
+
+		/**
+		 * Registers the ACF Fields for Sources
+		 * @author Cadie Stockman
+		 * @since 1.1.0
+		 * @return void
+		 */
+		public static function register_acf_fields() {
+			// Bail out if the function is missing.
+			if ( ! function_exists( 'acf_add_local_field_group' ) ) return;
+
+			// Create the field array.
+			$fields = array();
+
+			/**
+			 * Adds the Source Icon field
+			 */
+			$fields[] = array(
+				'key'               => 'field_5c9d07cbed834',
+				'label'             => 'Source Icon',
+				'name'              => 'source_icon',
+				'type'              => 'image',
+				'instructions'      => '',
+				'required'          => 1,
+				'conditional_logic' => 0,
+				'wrapper'           => array(
+					'width' => '',
+					'class' => '',
+					'id'    => '',
+				),
+				'return_format'     => 'url',
+				'preview_size'      => 'thumbnail',
+				'library'           => 'all',
+				'min_width'         => '',
+				'min_height'        => '',
+				'min_size'          => '',
+				'max_width'         => '',
+				'max_height'        => '',
+				'max_size'          => '',
+				'mime_types'        => '',
+			);
+
+			/**
+			 * Defines field group
+			 */
+			$field_group = array(
+				'key'                   => 'group_5c9d07be917b6',
+				'title'                 => 'Sources Taxonomy Fields',
+				'fields'                => $fields,
+				'location'              => array(
+					array(
+						array(
+							'param'    => 'taxonomy',
+							'operator' => '==',
+							'value'    => 'sources',
+						),
+					),
+				),
+				'menu_order'            => 0,
+				'position'              => 'normal',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen'        => '',
+				'active'                => true,
+				'description'           => '',
+
+			);
+
+			acf_add_local_field_group( $field_group );
+		}
 	}
 }
